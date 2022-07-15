48f21ee70c7910d9b420711a8cba3df5644f7d34
SzymonGil <66784070+SzymonGil@users.noreply.github.com>
2022-07-15 12:33:41 +0200

    Update and rename hello.py to hangman.py

---
+++ hangman.py
@@ -0,0 +1,97 @@
+
+
+import random
+
+hangmans = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_game = False
+words = ["ardvark", "baboon", "camel","apple","banana","cat","dog","home","grass","game"]
+choice = random.choice(words)
+lenght = len(choice)
+
+lives = 6
+
+
+all = []
+for _ in range(lenght):
+    all += "_"
+
+while not end_game:
+    guess = input("Guess a letter: ").lower()
+
+
+    for pos in range(lenght):
+        letter = choice[pos]
+        if letter == guess:
+            all[pos] = letter
+
+    if guess not in choice:
+        lives -= 1
+        if lives == 0:
+            end_game = True
+            print("You lose.")
+
+    
+    print(f"{' '.join(all)}")
+
+    
+    if "_" not in all:
+        end_game = True
+        print("You win.")
+
+    print(hangmans[lives])
